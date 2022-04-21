fc53dddd86f01fd612d688b0a599066c5047eaaf
shenzhen2017 <2868405029@qq.com>
2022-04-21 09:47:13 +0800

    MOD: ui detail

--- app/src/main/java/com/zj/compose/fps/MainActivity.kt
+++ app/src/main/java/com/zj/compose/fps/MainActivity.kt
@@ -4,18 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.zj.compose.fps.ui.page.list.ComposeListActivity
 import com.zj.compose.fps.ui.theme.ComposefpsTheme
 
@@ -25,12 +24,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposefpsTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    ItemList()
-                }
+
+                ItemList()
+
             }
         }
     }
@@ -44,9 +40,14 @@ fun ItemList() {
             .fillMaxWidth()
             .wrapContentHeight()
     ) {
-        Button(onClick = {
-            context.startActivity(Intent(context, ComposeListActivity::class.java))
-        }) {
+        Button(modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .height(44.dp)
+            .clip(RoundedCornerShape(16)),
+            onClick = {
+                context.startActivity(Intent(context, ComposeListActivity::class.java))
+            }) {
             Text(text = "ComposeList Fps测试")
         }
     }
