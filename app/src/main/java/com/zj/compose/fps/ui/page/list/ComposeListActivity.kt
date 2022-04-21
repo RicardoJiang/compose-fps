5c8228e4bfd4b94a1bfd12194e11b337da0db5ed
shenzhen2017 <2868405029@qq.com>
2022-04-21 22:34:48 +0800

    ADD: fpsmonitor

--- app/src/main/java/com/zj/compose/fps/ui/page/list/ComposeListActivity.kt
+++ app/src/main/java/com/zj/compose/fps/ui/page/list/ComposeListActivity.kt
@@ -3,20 +3,28 @@ package com.zj.compose.fps.ui.page.list
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.zj.compose.fps.ui.theme.ComposefpsTheme
+import com.zj.compose.fps.ui.widget.FpsMonitor
 
 class ComposeListActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeList()
+            Box {
+                ComposeList()
+                FpsMonitor(modifier = Modifier.align(Alignment.TopEnd).padding(16.dp))
+            }
         }
     }
 }
---
+++ app/src/main/java/com/zj/compose/fps/ui/widget/FpsMonitor.kt
@@ -0,0 +1,33 @@
+package com.zj.compose.fps.ui.widget
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FpsMonitor(modifier: Modifier) {
+    var fpsCount by remember { mutableStateOf(0) }
+    var fps by remember { mutableStateOf(0) }
+    var lastUpdate by remember { mutableStateOf(0L) }
+    Text(
+        text = fps.toString(), modifier = modifier
+            .size(30.dp), color = Color.Red, style = MaterialTheme.typography.body1
+    )
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            withFrameMillis { ms ->
+                fpsCount++
+                if (fpsCount == 5) {
+                    fps = (5000 / (ms - lastUpdate)).toInt()
+                    lastUpdate = ms
+                    fpsCount = 0
+                }
+            }
+        }
+    }
+}
